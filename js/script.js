eb7673368663b4de31406a6fe1ba3937488bfd84
Harshit Arora <71753965+vegeta2op@users.noreply.github.com>
2023-05-07 02:19:33 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-
-
 fetch("stud.json")
   .then(function(response) {
     return response.json();
@@ -8,6 +6,7 @@ fetch("stud.json")
     let placeholder = document.querySelector("#data-out");
     let out = "";
     for (let student of stud) {
+      let starred = student['Starred']  === 'Y' ? '' : 'checked';
       out += `
         <tr>
           <td>${student.Name}</td>
@@ -15,10 +14,33 @@ fetch("stud.json")
           <td>${student.Class}</td>
           <td>${student['Previous Year Rank']}</td>
           <td><button id="myBtn">View</button></td>
-          <td><input class="star" type="checkbox" title="bookmark page"></td>
+          <td><input class="star" type="checkbox" title="bookmark page" ${starred} data-id="${student.USN}"></td>
         </tr>
       `;
     }
 
     placeholder.innerHTML = out;
+
+let starInputs = document.querySelectorAll(".star");
+for (let studentIndex = 0; studentIndex < stud.length; studentIndex++) {
+  starInputs[studentIndex].addEventListener("change", function(event) {
+    let studentUSN = event.target.getAttribute('data-id');
+    let studentIndex = stud.findIndex(student => student.USN === studentUSN);
+    let newValue = event.target.checked ? 'N' : 'Y'; // swap the values
+    stud[studentIndex]['Starred'] = newValue;
+    let json = JSON.stringify(stud);
+    fetch('stud.json', {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: json
+    }).then(function(response) {
+      console.log('Success:', JSON.stringify(response));
+    }).catch(function(error) {
+      console.error('Error:', error);
+    });
+  });
+}
+
   });
