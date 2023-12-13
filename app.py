694b54de2abd28ca9eed5890ecc624788e09c55a
Harshit Arora <71753965+vegeta2op@users.noreply.github.com>
2023-12-13 23:24:28 +0530

    Version v1.01
    - added new Login/signup page
    - updated some back-end code
    - Added light /dark mode option 
    - added password and confirm password 
    - added session login for each user 
    - added email and password length requirements .
    
    Next Update will be Scheduled soon with lot of new UI updates and new features with stronger security and better UI .

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_login import LoginManager, UserMixin, login_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField,validators
 from wtforms.validators import DataRequired, Length, EqualTo
@@ -12,9 +12,10 @@ from flask_session import Session
 
 
 
+
 app = Flask(__name__)
 csrf = CSRFProtect(app)
-app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SECRET_KEY'] = '3893298h88932@298h23;32h'
 app.config['SESSION_TYPE'] = 'filesystem'
 Session(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///userpass.db'
@@ -42,9 +43,16 @@ def load_user(user_id):
 
 
 class RegistrationForm(FlaskForm):
-    email = StringField('email', validators=[DataRequired(), Length(min=6, max=120)])
-    password = PasswordField('password', validators=[DataRequired()])
-    submit = SubmitField('btn-l')
+    email = StringField('Email Address', validators=[validators.DataRequired(), validators.Email()])
+    password = PasswordField('Password', validators=[
+        validators.DataRequired(),
+        validators.Length(min=8, message="Password must be at least 8 characters long")
+    ])
+    re_password = PasswordField('Confirm Password', validators=[
+        validators.DataRequired(),
+        validators.EqualTo('password', message='Passwords must match')
+    ])
+    submit = SubmitField('Signup')
 
 
 
@@ -54,48 +62,54 @@ class LoginForm(FlaskForm):
     submit = SubmitField('login-btn')
 
 
-@app.route("/", methods=["GET", "POST"])
-def login():
-    form = LoginForm()
-    error_message = ""
-    if form.validate_on_submit():
-        print(form.email.data)
-        user = User.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
-            login_user(user)
-            session['email'] = user.email
-            return redirect('/dashboard')
-        else:
-            error_message = "Invalid email or password"
-    return render_template("login.html", form=form,error_message=error_message)
 
 
+# Route for handling the signup form submission
 @app.route("/signup", methods=["POST"])
 def signup():
-    form = RegistrationForm()
-    error_message = ""
-
-    if form.validate_on_submit():
+    form = RegistrationForm(request.form)
+    if form.validate():
+        # Handle the signup form submission
         email = form.email.data
         password = form.password.data
-        confirm_password = request.form.get('re-password')  # Get the confirm_password from the request
-        print(password,confirm_password)
+        confirm_password = form.re_password.data
+
         if password != confirm_password:
             error_message = "Passwords must match"
+            return redirect(url_for("home"))
         else:
             user = User.query.filter_by(email=email).first()
             if user:
                 error_message = "Email already exists"
+                return redirect(url_for("home"))
             else:
                 hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
                 new_user = User(email=email, password=hashed_password)
                 db.session.add(new_user)
                 db.session.commit()
                 login_user(new_user)
-                session['email'] = new_user.email
-                return redirect('/dashboard')
-    
-    return render_template("login.html", form=form, error_message=error_message)
+                return redirect('/dashboard')  # Redirect to your dashboard or desired page
+
+# Route for handling the login form submission
+@app.route("/", methods=["GET", "POST"])
+def login():
+    error_message=""
+    if request.method == "POST":
+        form = LoginForm(request.form)
+        if form.validate_on_submit():
+            print(form.email.data)
+            user = User.query.filter_by(email=form.email.data).first()
+            if user and bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                session['email'] = user.email
+                return redirect('/dashboard')  
+            else:
+                error_message = "Invalid email or password"
+        return render_template("login.html", registration_form=RegistrationForm(), login_form=form, error_message="Invalid email or password")
+    else:
+        # If it's a GET request, just render the login page
+        return render_template("login.html", registration_form=RegistrationForm(), login_form=LoginForm())
+
 app.register_blueprint(predictor_bp)
 app.register_blueprint(dashboard_bp)
 app.register_blueprint(student_bp)
